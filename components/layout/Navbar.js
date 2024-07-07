17b08dd6fdd3aeaededf9630ddd4206ee437032f
sabirof <128794677+sabirof@users.noreply.github.com>
2024-07-07 17:20:05 +0200

    navbar styled

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -7,19 +7,43 @@ const Navbar = () => {
   const { t } = useTranslation('common');
 
   return (
-    <header className="relative flex items-center justify-between w-full">
+    <header className="relative flex items-center justify-between w-full pt-20">
       {/* Navbar Section */}
-      <nav className="bg-primary p-4 w-2/3">
-        <div className="container mx-auto flex justify-between items-center">
-          <ul className="text-white flex gap-4">
-            <li><Link href="/" legacyBehavior>{t('navbar.home')}</Link></li>
-            <li><Link href="/vision" legacyBehavior>{t('navbar.vision')}</Link></li>
-            <li><Link href="/project" legacyBehavior>{t('navbar.project')}</Link></li>
-            <li><Link href="/news" legacyBehavior>{t('navbar.news')}</Link></li>
-            <li><Link href="/about" legacyBehavior>{t('navbar.about')}</Link></li>
-            <li><Link href="/contact" legacyBehavior>{t('navbar.contact')}</Link></li>
+      <nav className="w-2/3">
+        <div className="container mx-auto flex justify-start items-center gap-4">
+          <ul className="text-black flex gap-6 text-lg">
+            <li>
+              <Link href="/" className="hover:bg-purple-200 p-2 rounded-md transition">
+                {t('navbar.home')}
+              </Link>
+            </li>
+            <li>
+              <Link href="/vision" className="hover:bg-purple-200 p-2 rounded-md transition">
+                {t('navbar.vision')}
+              </Link>
+            </li>
+            <li>
+              <Link href="/project" className="hover:bg-purple-200 p-2 rounded-md transition">
+                {t('navbar.project')}
+              </Link>
+            </li>
+            <li>
+              <Link href="/news" className="hover:bg-purple-200 p-2 rounded-md transition">
+                {t('navbar.news')}
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="hover:bg-purple-200 p-2 rounded-md transition">
+                {t('navbar.about')}
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className="hover:bg-purple-200 p-2 rounded-md transition">
+                {t('navbar.contact')}
+              </Link>
+            </li>
           </ul>
-          <div className="flex gap-4">
+          <div className="ml-2">
             <LanguageSwitcher />
           </div>
         </div>
@@ -27,7 +51,7 @@ const Navbar = () => {
 
       {/* Logo Section */}
       <div className="absolute right-0 top-1/2 transform -translate-y-1/2 pr-4 pt-40">
-        <Link href="/" legacyBehavior>
+        <Link href="/">
           <Image src="/images/logo2.png" alt="Logo" width={300} height={300} priority />
         </Link>
       </div>
