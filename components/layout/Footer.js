fe9f1acf6cbd0244a591506256106de1599c4988
sabirof <128794677+sabirof@users.noreply.github.com>
2024-08-06 14:13:37 +0200

    footer update

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -21,19 +21,22 @@ const Footer = () => {
           
           <div className="w-full flex flex-col items-center p-4 rounded-lg newsletter-section">
             <h3 className="text-3xl md:text-2xl font-bold mb-4 text-center">{t('newsletter.newsletterTitle')}</h3>
+            <div className="w-full mb-8 p-4 rounded-sm text-center">
+  <p className="text-base md:text-basefont-bold leading-relaxed text-white">{t('footer.stayUpdated')}</p>
+</div>
             <Link href="/newsletter">
               <div className="flex items-center justify-between bg-secondary text-primary px-10 py-5 rounded-lg transition duration-200 hover:bg-green-400 focus:outline-none text-2xl">
+                
                 <span>{t('newsletter.newsletterSubscribe')}</span>
                 <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" d="M12.293 9.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 15H4a1 1 0 110-2h10.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
                 </svg>
               </div>
+              
             </Link>
             </div>
             {/* Text Section */}
-        <div className="w-full mb-8 p-4 rounded-sm text-center">
-  <p className="text-base md:text-basefont-bold leading-relaxed text-white">{t('footer.stayUpdated')}</p>
-</div>
+     
           
         </div>
       </div>
